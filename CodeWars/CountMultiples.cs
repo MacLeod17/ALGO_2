e0efa1f3be793ca4551d8a8bf8c457e9e03e174a
Garrick Kilpack <garrickkilpack@hotmail.com>
2021-05-03 10:01:36 -0600

    Created CodeWars project

---
+++ CodeWars/CountMultiples.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars
+{
+    public class CountMultiples
+    {
+        public static long CountSpecMult(long n, long mxval)
+        {
+            // your code
+
+            return 0;
+        }
+    }
+}
