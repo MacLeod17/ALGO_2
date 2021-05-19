63fdc17fe41ad81349ce0435e62f9835e7825bd5
Garrick Kilpack <garrickkilpack@hotmail.com>
2021-05-19 00:26:54 -0600

    AVL Tree initial setup

--- AlgoDataStructures/AVLTree/AVLNode.cs
+++ AlgoDataStructures/AVLTree/AVLNode.cs
@@ -2,36 +2,91 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace AlgoDataStructures.BinarySearchTree
+namespace AlgoDataStructures
 {
-    public class BinaryNode<T> where T : IComparable
+    public class AVLNode<T> where T : IComparable
     {
-        private BinaryNode<T> Left { get; set; }
-        private BinaryNode<T> Right { get; set; }
-        private BinaryNode<T> Parent { get; set; }
+        private AVLNode<T> Left { get; set; }
+        private AVLNode<T> Right { get; set; }
+        private AVLNode<T> Parent { get; set; }
 
         public T Data { get; set; }
 
-        public BinaryNode() { }
-        public BinaryNode(T val)
+        public AVLNode() { }
+        public AVLNode(T val)
         {
             Data = val;
         }
 
+        #region AVL Methods
         public void Add(T val)
         {
+            // Not Modified Yet
             if (val.CompareTo(Data) < 0)
             {
-                if (Left == null) Left = new BinaryNode<T>(val) { Parent = this }; 
+                if (Left == null) Left = new AVLNode<T>(val) { Parent = this };
                 else Left.Add(val);
             }
             if (val.CompareTo(Data) > 0)
             {
-                if (Right == null) Right = new BinaryNode<T>(val) { Parent = this }; 
+                if (Right == null) Right = new AVLNode<T>(val) { Parent = this };
                 else Right.Add(val);
             }
         }
 
+        public AVLNode<T> RR_Rotation(AVLNode<T> unbalanced)
+        {
+            AVLNode<T> pivot = unbalanced.Left;
+
+            unbalanced.Left = pivot.Right;
+            pivot.Right = unbalanced;
+
+            return pivot;
+        }
+
+        public AVLNode<T> LL_Rotation(AVLNode<T> unbalanced)
+        {
+            AVLNode<T> pivot = unbalanced.Right;
+
+            unbalanced.Right = pivot.Left;
+            pivot.Left = unbalanced;
+
+            return pivot;
+        }
+
+        public AVLNode<T> RL_Rotation(AVLNode<T> unbalanced)
+        {
+            // Not Modified Yet
+            return default;
+        }
+
+        public AVLNode<T> LR_Rotation(AVLNode<T> unbalanced)
+        {
+            // Not Modified Yet
+            return default;
+        }
+
+        public int ToArray(T[] newArrayWithData, int index)
+        {
+            // Not Modified Yet
+
+            // Left Root Right
+            if (Left != null)
+            {
+                index = Left.ToArray(newArrayWithData, index);
+            }
+            // Assign Value To Array
+            newArrayWithData[index++] = Data;
+            if (Right != null)
+            {
+                index = Right.ToArray(newArrayWithData, index);
+            }
+
+            return index;
+        }
+        #endregion
+
+        #region Unmodified BST Methods
         // (Left, Root, Right)
         public string InOrder()
         {
@@ -89,16 +144,16 @@ namespace AlgoDataStructures.BinarySearchTree
             if (Data.Equals(val)) return true;
 
             //if val < data && left exists go left
-                //return the return boolean
+            //return the return boolean
             if (Left != null && val.CompareTo(Data) < 0) return Left.Contains(val);
             //if val > data && right exists go right
-                //return the return boolean
+            //return the return boolean
             if (Right != null && val.CompareTo(Data) > 0) return Right.Contains(val);
 
             return false;
         }
 
-        public BinaryNode<T> Remove(BinaryNode<T> node, T val)
+        public AVLNode<T> Remove(AVLNode<T> node, T val)
         {
             if (val.CompareTo(node.Data) < 0)
             {
@@ -125,28 +180,11 @@ namespace AlgoDataStructures.BinarySearchTree
             return node;
         }
 
-        public T MaxValue(BinaryNode<T> node)
+        public T MaxValue(AVLNode<T> node)
         {
             while (node.Right != null) node = node.Right;
 
             return node.Data;
-        }
-
-        public int ToArray(T[] newArrayWithData, int index)
-        {
-            // Left Root Right
-            if (Left != null)
-            {
-                index = Left.ToArray(newArrayWithData, index);
-            }
-            // Assign Value To Array
-            newArrayWithData[index++] = Data;
-            if (Right != null)
-            {
-                index = Right.ToArray(newArrayWithData, index);
-            }
-
-            return index;
         }
 
         public int Height()
@@ -163,13 +201,14 @@ namespace AlgoDataStructures.BinarySearchTree
             int rightMaxSize = 0;
 
             //check if left not null
-                //leftMaxSize = left.Height()
+            //leftMaxSize = left.Height()
             if (Left != null) leftMaxSize = Left.Height();
             //check if right not null
-                //rightMaxSize = right.Height()
+            //rightMaxSize = right.Height()
             if (Right != null) rightMaxSize = Right.Height();
 
             return Math.Max(leftMaxSize, rightMaxSize) + 1;
         }
+        #endregion
     }
 }
---
+++ AlgoDataStructures/AVLTree/AVLTree.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoDataStructures
+{
+    public class AVLTree<T> where T : IComparable
+    {
+        AVLNode<T> root;
+
+        public int Count { get; set; }
+
+        public void Add(T val)
+        {
+            if (root == null) root = new AVLNode<T>(val);
+            else root.Add(val);
+            Count++;
+        }
+
+        public bool Contains(T val)
+        {
+            return root != null ? root.Contains(val) : false;
+        }
+
+        public void Remove(T val)
+        {
+            if (root != null && root.Contains(val))
+            {
+                root = root.Remove(root, val);
+                Count--;
+            }
+        }
+
+        public void Clear()
+        {
+            root = null;
+            Count = 0;
+        }
+
+        public string InOrder()
+        {
+            string inOrder = root != null ? root.InOrder() : string.Empty;
+
+            return inOrder.Remove(inOrder.Length - 2);
+        }
+
+        public string PreOrder()
+        {
+            string preOrder = root != null ? root.PreOrder() : string.Empty;
+
+            return preOrder.Remove(preOrder.Length - 2);
+        }
+
+        public string PostOrder()
+        {
+            string postOrder = root != null ? root.PostOrder() : string.Empty;
+
+            return postOrder.Remove(postOrder.Length - 2);
+        }
+
+        public int Height()
+        {
+            return root != null ? root.Height() : 0;
+        }
+
+        public T[] ToArray()
+        {
+            if (root != null)
+            {
+                T[] arr = new T[Count];
+
+                root.ToArray(arr, 0);
+
+                return arr;
+            }
+
+            return null;
+        }
+    }
+}
