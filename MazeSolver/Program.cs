db668a66b49ebc98ac58ff1d6cffbf084d30f86f
Garrick Kilpack <garrickkilpack@hotmail.com>
2021-05-24 08:50:43 -0600

    Started the MazeSolver lab

---
+++ MazeSolver/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace MazeSolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter File Path: ");
+            string path = Console.ReadLine();
+
+            IEnumerable<string> result = File.ReadLines(path);
+
+            foreach (string line in result)
+            {
+
+            }
+        }
+    }
+}
